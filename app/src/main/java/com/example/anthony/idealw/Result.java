68623f41ed9d087ac6874ecd8e69b792ca2c728c
Anthony <anthony.arg20@gmail.com>
2018-07-23 03:15:30 -0400

    add improves

--- app/src/main/java/com/example/anthony/idealw/Result.java
+++ app/src/main/java/com/example/anthony/idealw/Result.java
@@ -1,9 +1,12 @@
 package com.example.anthony.idealw;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class Result extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,6 +21,7 @@ public class Result extends AppCompatActivity implements View.OnClickListener {
         TextView txtHeight = findViewById(R.id.txtViewResult2);
         TextView txtWeight = findViewById(R.id.txtViewResult3);
         TextView txtImc = findViewById(R.id.txtViewResult4);
+        TextView principalResultWeight = findViewById(R.id.txtViewResult5);
 
         char sex = getIntent().getCharExtra("sex", 'm');
         double height = getIntent().getDoubleExtra("Height",0);
@@ -35,20 +39,42 @@ public class Result extends AppCompatActivity implements View.OnClickListener {
 
         double imc = weight/(height*height);
 
+        DecimalFormat df = new DecimalFormat("#.00");
+        String prw;
+
         if (sex == 'f'){
             txtSex.setText(getString(R.string.resultSex , "Femenino"));
         }else {
             txtSex.setText(getString(R.string.resultSex , "Masculino"));
         }
-        txtHeight.setText(getString(R.string.resultHeight, height, heightMeasure));
-        txtWeight.setText(getString(R.string.resultWeight, weight, weightMeasure));
-        txtImc.setText(getString(R.string.resultImc, imc));
 
+        if(imc<18.5){
+            prw = "Peso insuficiente";
+        }else if(imc>18.5 & imc<24.9){
+            prw = "Peso normal(ideal)";
+        }else if(imc>25 & imc<26.9){
+            prw = "Puede haber sobrepeso grado I";
+        }else if(imc>27 & imc<29.9){
+            prw = "Sobrepeso tipo I (preobesidad)";
+        }else if(imc>30 & imc<34.9){
+            prw = "Obesidad tipo I (leve)";
+        }else if(imc>35 & imc<39.9){
+            prw = "Obesidad tipo II (moderada)";
+        }else if(imc>40 & imc<49.9){
+            prw = "Obesidad tipo III (mórbida)";
+        }else{
+            prw = "Obesidad extrema";
+        }
 
+        txtHeight.setText(getString(R.string.resultHeight, df.format(height), heightMeasure));
+        txtWeight.setText(getString(R.string.resultWeight, df.format(weight), weightMeasure));
+        txtImc.setText(getString(R.string.resultImc, df.format(imc)));
+        principalResultWeight.setText(getString(R.string.principalResultWeight, prw));
     }
 
     @Override
     public void onClick(View view) {
-
+        Intent intentM = new Intent(this, MainActivity.class);
+        startActivity(intentM);
     }
 }

1d4f1d0e517edd8b986442dbe1272e1a28597952
Anthony <anthony.arg20@gmail.com>
2018-07-24 03:23:43 -0400

    add new video background

--- app/src/main/java/com/example/anthony/idealw/MainActivity.java
+++ app/src/main/java/com/example/anthony/idealw/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.anthony.idealw;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.transition.Slide;
@@ -10,16 +12,37 @@ import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    VideoView video;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.btnMain).setOnClickListener(this);
+        video = findViewById(R.id.videoView);
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.running);
+        video.setVideoURI(uri);
 
+        video.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
+
+        video.start();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(!video.isPlaying()) {
+            video.resume();
+        }
     }
 
     @Override
